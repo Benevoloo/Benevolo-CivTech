899f840f8b2a4ae74a4211c51ac54db5e58527b8
Jahlapeno1 <jimenezaot@gmail.com>
2024-10-16 03:53:45 -0400

    CRUD for tasks

--- server/models/Neighbor.js
+++ server/models/Neighbor.js
@@ -1,7 +1,7 @@
 const knex = require('../db/knex');
 const authUtils = require('../utils/auth-utils');
 
-class User {
+class Neighbor {
   #passwordHash = null; // a private property
 
   // This constructor is NOT how a controller creates a new user in the database.
@@ -9,10 +9,12 @@ class User {
   // static methods to hide the hashed password of users before sending user data 
   // to the client. Since we want to keep the #passwordHash property private, we 
   // provide the isValidPassword instance method as a way to indirectly access it.
-  constructor({ id, username, password_hash}) {
+  constructor({ id, username, password_hash, zipcode, bio}) {
     this.id = id;
     this.username = username;
     this.#passwordHash = password_hash;
+    this.zipcode = zipcode;
+    this.bio = bio;
   }
 
   // This instance method takes in a plain-text password and returns true if it matches
@@ -22,32 +24,37 @@ class User {
   );
 
 
-
   // Fetches ALL users from the users table, uses the constructor
   // to format each user (and hide their password hash), and returns.
   static async list() {
-    const query = `SELECT * FROM users`;
+    const query = `SELECT * FROM neighbors`;
     const result = await knex.raw(query);
-    return result.rows.map((rawUserData) => new User(rawUserData));
+    return result.rows.map((rawUserData) => new Helper(rawUserData));
+  }
+
+  static async listneighborsByZip(zipcode) {
+    const query = `SELECT * FROM neighbors WHERE zipcode = ?`;
+    const result = await knex.raw(query, [zipcode]);
+    return results.rows.map((rawUserData) => new Helper(rawUserData))
   }
 
   // Fetches A single user from the users table that matches
   // the given user id. If it finds a user, uses the constructor
   // to format the user and returns or returns null if not.
   static async find(id) {
-    const query = `SELECT * FROM users WHERE id = ?`;
+    const query = `SELECT * FROM neighbors WHERE id = ?`;
     const result = await knex.raw(query, [id]);
     const rawUserData = result.rows[0];
-    return rawUserData ? new User(rawUserData) : null;
+    return rawUserData ? new Helper(rawUserData) : null;
   }
 
 
   // Same as above but uses the username to find the user
   static async findByUsername(username) {
-    const query = `SELECT * FROM users WHERE username = ?`;
+    const query = `SELECT * FROM neighbors WHERE username = ?`;
     const result = await knex.raw(query, [username]);
     const rawUserData = result.rows[0];
-    return rawUserData ? new User(rawUserData) : null;
+    return rawUserData ? new Helper(rawUserData) : null;
   }
 
   // Hashes the given password and then creates a new user
@@ -57,30 +64,34 @@ class User {
     // hash the plain-text password using bcrypt before storing it in the database
     const passwordHash = await authUtils.hashPassword(password);
 
-    const query = `INSERT INTO users (username, password_hash)
+    const query = `INSERT INTO neighbors (username, password_hash)
       VALUES (?, ?) RETURNING *`;
     const result = await knex.raw(query, [username, passwordHash]);
     const rawUserData = result.rows[0];
-    return new User(rawUserData);
+    return new Helper(rawUserData);
   }
 
   // Updates the user that matches the given id with a new username.
   // Returns the modified user, using the constructor to hide the passwordHash. 
-  static async update(id, username) {
+  static async update(username, contact_info, zipcode, bio, id) {
     const query = `
-      UPDATE users
-      SET username=?
+      UPDATE neighbors
+      SET 
+      username = COALESCE (?, username),
+      contact_info = COALESCE (?, contact_info),
+      zipcode = COALESCE (?, zipcode),
+      bio = COALESCE (?, bio)
       WHERE id=?
       RETURNING *
     `
-    const result = await knex.raw(query, [username, id])
+    const result = await knex.raw(query, [username, contact_info, zipcode, bio, id])
     const rawUpdatedUser = result.rows[0];
-    return rawUpdatedUser ? new User(rawUpdatedUser) : null;
+    return rawUpdatedUser ? new Helper(rawUpdatedUser) : null;
   };
 
   static async deleteAll() {
-    return knex('users').del()
+    return knex('neighbors').del()
   }
 }
 
-module.exports = User;
+module.exports = Neighbor;
