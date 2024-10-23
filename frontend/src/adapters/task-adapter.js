9a335556a6588ddf4dd05a4851c3ca76b4c0125e
Fiona Baci <fionabaci04@gmail.com>
2024-10-23 09:57:58 -0400

    changes

--- frontend/src/adapters/task-adapter.js
+++ frontend/src/adapters/task-adapter.js
@@ -5,3 +5,7 @@ const baseUrl = '/api';
 export const checkForInterest = async (task_id) => {
   return fetchHandler(`${baseUrl}/interests/${task_id}`);
 }
+
+export const makeTask = async (title, body, zipcode, status = null, created_at = new Date(), expiration_date, neighbor_id) => {
+    return fetchHandler(`${baseUrl}/tasks`, getPostOptions({title, body, zipcode, status, created_at, expiration_date, neighbor_id}))
+}
