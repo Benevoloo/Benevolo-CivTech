ebbf9231559d00bf12c77488aec49ae58f71ec19
Jahlapeno1 <jimenezaot@gmail.com>
2024-10-29 11:09:48 -0400

    Fixed interests button

--- frontend/src/components/InterestsButton.jsx
+++ frontend/src/components/InterestsButton.jsx
@@ -45,7 +45,7 @@
 //         <ul>
 //           {numOfPeople.map((helper) => {
 //             <li>
-              
+
 //             </li>
 //           })}
 //         </ul>
@@ -74,43 +74,34 @@ import { checkForInterest } from "../adapters/task-adapter";
 import Modal from "./Modal";
 
 const InterestButton = ({ task_id }) => {
- 
-  // const taskInterests = async (task_id) => {
-  //   const [data, error] = await checkForInterest(task_id);
-  //   if (error) {
-  //     console.error("Error checking interests:", error);
-  //   } else {
-  //     return data; // Return the data retrieved from the API.
-  //   }
-  // };
 
-  // useEffect(() => {
-  //   const fetchInterests = async () => {
-  //     try {
-  //       const interests = await taskInterests(task_id);
-  //       setInterestedPeople(interests); // Set the list of interested people.
-  //     } catch (error) {
-  //       console.error("Error fetching interests on load:", error);
-  //     }
-  //   };
-
-  //   fetchInterests();
-  // }, [task_id]);
-
-  // const handleInterestsDrop = (e) => {
-  //   e.preventDefault();
-  //   if (interestedPeople.length === 0) {
-  //     return;
-  //   } else {
-  //     setIsOpen(!isOpen); // Toggle between open and close
-  //   }
-  // };
-
-            {/* {interestedPeople.map((helper, index) => (
-            <li key={index}>{helper.helperId.name}</li>
-          ))} */}
   const [interestedPeople, setInterestedPeople] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const taskInterests = async (task_id) => {
+    const [data, error] = await checkForInterest(task_id);
+    if (error) {
+      console.error("Error checking interests:", error);
+    } else {
+      return data; // Return the data retrieved from the API.
+    }
+  };
+
+  useEffect(() => {
+    const fetchInterests = async () => {
+      try {
+        const interests = await taskInterests(task_id);
+        setInterestedPeople(interests); // Set the list of interested people.
+        console.log()
+      } catch (error) {
+        console.error("Error fetching interests on load:", error);
+      }
+    };
+
+    fetchInterests();
+  }, [task_id]);
+
+
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -119,16 +110,16 @@ const InterestButton = ({ task_id }) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
- 
+
 
   return (
     <div>
       <button onClick={openModal} className="mt-5 w-full bg-orange-600 text-white rounded-lg hover:bg-orange-700">
-    {[].length} helpers interested
-       </button>
+        {interestedPeople.length} helpers interested
+      </button>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-      <p>Will change descr. when i get back home.</p>
-      <button onClick={closeModal}>Close</button>
+        <p>Will change descr. when i get back home.</p>
+        <button onClick={closeModal}>Close</button>
       </Modal>
     </div>
   );
