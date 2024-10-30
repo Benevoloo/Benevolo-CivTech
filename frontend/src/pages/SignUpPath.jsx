5b6fb8e19bd6b2b151a116dd6b3a725a8f801fbf
Fiona Baci <fionabaci04@gmail.com>
2024-10-30 02:14:19 -0400

    added paths to buttons, made the nav-bar sign in page better

--- frontend/src/pages/SignUpPath.jsx
+++ frontend/src/pages/SignUpPath.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 import SignUpOptions from "../components/SignUpOptions";
 
 const SignUpPath = () => {
+  // Creating an official page that is shown to the user when they click "sign up" in the header
+  return (
+    <div className="max-w-6xl mx-auto py-16 px-6">
+      <h2 className="text-5xl font-bold text-center text-teal-900 mb-12">
+        Ready to Join A Greater Cause?
+      </h2>
 
-    //just creating an official page that is shown to the user when they click "sign up" in the header
-    return (
+      <SignUpOptions />
+    </div>
+  );
+};
 
-        <>
-            <h2 id="signUpPage-Header">Ready to Join A Greater Cause?</h2>
-
-            <SignUpOptions />
-        </>
-    )
-}
-
-export default SignUpPath
+export default SignUpPath;
